d923c3545b68d1815375eaecf7aa202cd8ef841d
Luis00444 <152845104+Luis00444@users.noreply.github.com>
2023-12-04 09:18:12 +0100

    question 6 to test if it works

--- enseash.c
+++ enseash.c
@@ -11,6 +11,7 @@
 #define MESSAGE "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\n"
 #define TERMINAL_TAG "enseash % "
 #define BUFFER_LEN 100
+#define TOKEN_LEN 20
 #define INT2POINTER(a) ((char*)(intptr_t)(a))
 
 void exitWithError(char* message) {
@@ -33,37 +34,49 @@ ssize_t readSTDin(char* buffer) {
     return readed;
 }
 
-void split(char* in, char** out){
-    char readed;
-    int spaceCounter = 0;
-    char word[100];
-    char counter = 0;
-    do{
-        readed = in[counter];
-        //if (readed == ' '){
+void split(char* in, char out[TOKEN_LEN][BUFFER_LEN]){
+    char delimiter[] = " ";
+    char* tokenPointer;
+    int counter = 0;
 
-        //}
-    } while (readed != '\0');
+    tokenPointer = strtok(in,delimiter);
+    while(tokenPointer != NULL){
+        strcpy(out[counter], tokenPointer);
+        counter++;
+        tokenPointer = strtok(NULL,delimiter);
+    }
+
 }
 
 int main(int argc, char* argv[]) {
-    bool control = true;
-    char buffer[BUFFER_LEN];
-    char** toTerminal;
-    int bufferReaded;
-    int status;
-    int exit;
-    bool isReturn;
+
+    //Variables
+    bool control = true;            //control of the mainloop
+    char buffer[BUFFER_LEN];        //the buffer of the mainloop
+    char** toTerminal;              //the expresion that goes to terminal
+    int status;                     //status for the wait
+    int exit;                       //the value of the return of signal
+    bool isReturn;                  //1 if return, 0 if signal
+
+    //mainloop
     do {
+        //write the tag and wait for input
         writeSTDout(TERMINAL_TAG);
         ssize_t read = readSTDin(buffer);
+
+        //compare if it is one especial case, to exit as an example.
         if ((strcmp("exit", buffer) == 0) || read == 0){
             writeSTDout("Bye\n");
             exit(EXIT_SUCCESS);
         }else {
-            //split(buffer, toTerminal);
+            //if not, it execute the command
+            split(buffer, toTerminal);          //split in tokens
+            //the fork for the exec
             pid_t pid;
             pid = fork();
+
+            //if is the parent, it will wait and read the exit status
+            //of the child when it dies.
             if (pid > 0){
                 wait(&status);
                 if(WIFEXITED(status)) {
@@ -75,8 +88,11 @@ int main(int argc, char* argv[]) {
                     isReturn = 0;
                 }
             }
+            //if is the child, it will execute the command of the
+            //buffer
             else if (pid == 0){
-                int exitValue = execlp(buffer, buffer, (char*)NULL);
+                //int exitValue = execlp(buffer, buffer, (char*)NULL);
+                int exitValue = execvp(buffer, toTerminal);
                 if (exitValue == -1) {
                     writeSTDout("enseash ");
                     writeSTDout("[exit:-1]");
